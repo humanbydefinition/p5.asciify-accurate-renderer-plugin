20d2c5b54acbcde6adf58591cf1166776919572e
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2025-05-13 21:47:25 +0200

    Updated example sketch

--- src/example/sketch.js
+++ src/example/sketch.js
@@ -1,32 +1,56 @@
+/**
+ * @name Accurate-plugin-renderer-example
+ * @description Basic example applying 'accurate' plugin renderer to a p5.js canvas with p5.asciify.
+ * @author humanbydefinition
+ * @link https://github.com/humanbydefinition/p5.asciify
+ * 
+ * This example demonstrates how to apply the 'accurate' plugin renderer to a p5.js canvas.
+ * An image (CC0 licensed) is displayed on the canvas and asciified using the 'accurate' renderer.
+ * 
+ * Attribution:
+ * - Brutalist high-rise building image by @AliImran on pexels.com:
+ *  - https://www.pexels.com/photo/black-and-white-photo-of-a-brutalist-high-rise-building-17209382/
+ */
+
 import p5 from 'p5';
 import { p5asciify } from 'p5.asciify';
 
 import { AccurateRendererPlugin } from '../plugin/AccurateRendererPlugin';
+import ExampleImage from './brutalist-high-rise-building.jpeg';
 
 const sketch = new p5((p) => {
 
+    let asciifier;
+    let img;
+
+    p.preload = () => {
+        img = p.loadImage(ExampleImage);
+    };
+
     p.setup = () => {
+        p.setAttributes('antialias', false);
         p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
 
         p5asciify.registerPlugin(AccurateRendererPlugin);
     };
 
     p.setupAsciify = () => {
-        p5asciify.asciifier().renderers().get("brightness").update({
-            enabled: false,
+        asciifier = p5asciify.asciifier();
+
+        asciifier.renderers().get("brightness").disable();
+
+        asciifier.renderers().add("accurate", "accurate");
+
+        asciifier.renderers().get("accurate").update({
+            characters: asciifier.fontManager.characters.map(charObj => charObj.character).join(''),
+            characterColorMode: "sampled",
+            backgroundColorMode: "sampled",
         });
-
-        p5asciify.asciifier().renderers().add("accurate", "accurate", { enabled: true});
-
-        console.log(p5asciify.asciifier().renderers().getAvailableRendererTypes());
     };
 
     p.draw = () => {
-        p.background(0);
-        p.rotateX(p.frameCount * 0.02);
-        p.rotateY(p.frameCount * 0.02);
-        p.normalMaterial();
-        p.box(400);
+        p.clear();
+        p.image(img, -p.windowWidth / 2, -p.windowHeight / 2);
     };
 
     p.windowResized = () => {
